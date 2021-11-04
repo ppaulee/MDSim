5a5bc206e4e27430be935141236eef304cc47cf2
mikmik30 <ge35doz@mytum.de>
2021-11-04 08:41:59 +0100

    added doxygen option

--- cmake/modules/doxygen.cmake
+++ cmake/modules/doxygen.cmake
@@ -1,5 +1,16 @@
 # make doc_doxygen optional if someone does not have / like doxygen
 
 # TODO: create CMake build option for the target.
-
+option(BUILD_DOXY "Build documentation" OFF)
 # TODO: Add a custom target for building the documentation.
+if (BUILD_DOXY)
+    find_package(Doxygen)
+    if (DOXYGEN_FOUND)
+        add_custom_target(doxyDoc COMMAND ${DOXYGEN_EXECUTABLE} "${CMAKE_BINARY_DIR}/../Doxyfile"
+                WORKING_DIRECTORY ${CMAKE_HOME_DIRECTORY}
+                COMMENT "Building user's documentation into doxyDoc build dir..."
+                )
+    else (DOXYGEN_FOUND)
+        message("Doxygen need to be installed to generate the doxygen documentation")
+    endif (DOXYGEN_FOUND)
+endif (BUILD_DOXY)
