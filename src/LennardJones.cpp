76ecbf0629123f9a29a17aa5f24d8760b665bb5e
Jonas Haegele <jonas@haegele-online.de>
2021-11-14 15:25:50 +0100

    Fixed error in force calculation

--- src/LennardJones.cpp
+++ src/LennardJones.cpp
@@ -9,7 +9,7 @@ std::array<double, 3> LennardJones::calculateF(Particle p1, Particle p2) {
     double norm = ArrayUtils::L2Norm(p1.getX() - p2.getX());
     double temp = pow(sigma / norm, 6);
     temp = temp - 2 * pow(sigma / norm, 12);
-    temp = temp * (24 * epsilon) / (norm * norm);
+    temp = -1 * temp * (24 * epsilon) / (norm * norm);
     std::array<double, 3> vec = temp * (p1.getX() - p2.getX());
     return vec;
 }
