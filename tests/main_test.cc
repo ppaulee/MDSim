7bd2fe1df2cb84605f28aef0c77ff46d8f144bdb
Paul Ungermann <paul.ungermann.tum.de>
2021-11-14 15:49:47 +0100

    Fixed LennardJonesPotential Test and added new test for StoermerVerlet

--- tests/main_test.cc
+++ tests/main_test.cc
@@ -3,15 +3,30 @@
 //#include "../src/utils/ArrayUtils.h"
 #include "../src/Particle.h"
 #include "../src/LennardJones.h"
+#include "../src/StoermerVerlet.h"
 #include "../src/Particle.cpp"
 #include "../src/LennardJones.cpp"
+#include "../src/StoermerVerlet.cpp"
 
 
-// Testing Lennard Jones Potential against by hand calculated values
+// Testing Lennard Jones Potential against hand-calculated values
 TEST(ForceCalculation, LennardJonesPotential) {
     ForceCalculation *algorithm = new LennardJones(5, 1);
     auto *p1 = new Particle({0,0,0},{1,0,0},1,0);
     auto *p2 = new Particle({0,2,0},{1,1,0},1,0);
     std::array<double, 3> vec = algorithm->calculateF(*p1, *p2);
-    ASSERT_EQ(-0.908203125, vec[1]);
+    ASSERT_EQ(0, vec[0]);
+    ASSERT_EQ(0.908203125, vec[1]);
+    ASSERT_EQ(0, vec[2]);
+}
+
+// Testing Stoermer Verlet against hand-calculated values
+TEST(ForceCalculation, StoermerVerlet) {
+    ForceCalculation *algorithm = new StoermerVerlet();
+    auto *p1 = new Particle({0,0,0},{1,0,0},1,0);
+    auto *p2 = new Particle({0,2,0},{1,1,0},1,0);
+    std::array<double, 3> vec = algorithm->calculateF(*p1, *p2);
+    ASSERT_EQ(0, vec[0]);
+    ASSERT_EQ(0.25, vec[1]);
+    ASSERT_EQ(0, vec[2]);
 }
