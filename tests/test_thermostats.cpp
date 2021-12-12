dd1254d7487acc7856f169f2b53beba1dfb6acbe
Paul <paulungermann@web.de>
2021-12-12 14:32:02 +0100

    Added new test for thermostats

--- tests/test_thermostats.cpp
+++ tests/test_thermostats.cpp
@@ -153,6 +153,43 @@ TEST(Thermostats, cool_down) {
 }
 
 /**
+ * Cools down the simulation and compare the end temperature with the expected temperature. Here we use a step size of 2 and a end_time of 6. Here
+ * we have a multiple of 2 as the end_time
+ */
+TEST(Thermostats, cool_down_steps) {
+    auto cells = new LinkedCells({40, 40, 0}, 1, 1, 5);
+    auto p1 = new Particle({0, 0, 0}, {0, 1, 0}, 1, 1);
+    auto p2 = new Particle({1, 1, 0}, {1, 1, 0}, 1, 1);
+
+    cells->insert(*p1);
+    cells->insert(*p2);
+
+    double delta_t = 1;
+    double end_t = 6;
+    int initialTemp = 40;
+    int stepSize = 2;
+    int dimensions = 2;
+    int targetTemp = 20;
+    double epsilon = 5;
+    double sigma = 1;
+
+    double current_time = 0;
+    auto algorithm = new LennardJones(epsilon, sigma);
+
+    auto thermo = new Thermostats(*cells, delta_t, end_t, initialTemp, stepSize, dimensions, targetTemp);
+    thermo->calcCurrentTemperature(*cells);
+    thermo->adjustTemperature(*cells, current_time);
+    while (current_time < end_t) {
+        cells->simulate(algorithm, delta_t);
+        thermo->calcCurrentTemperature(*cells);
+        thermo->adjustTemperature(*cells, current_time);
+        thermo->calcCurrentTemperature(*cells);
+        current_time += delta_t;
+    }
+    EXPECT_NEAR(thermo->getCurrentTemperature(), 20, 0.001);
+}
+
+/**
  * Hold the temperature of the simulation and compares the end temperature with the initial temperature
  */
 TEST(Thermostats, hold) {
